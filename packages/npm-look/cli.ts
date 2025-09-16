9a4c53781811afb0fbeabba0a38fe0f468bc6622
Sriman <136729116+TenEplaysOfficial@users.noreply.github.com>
2025-09-16 17:06:06 +0530

    wip

--- packages/npm-look/cli.ts
+++ packages/npm-look/cli.ts
@@ -1,3 +1,4 @@
+import { packageLook, userLook } from './index.js';
 import pkg from './package.json' with { type: 'json' };
 
 const args = process.argv.slice(2);
@@ -17,6 +18,7 @@ Options:
   -v, --version    Show version number
 
 Examples:
+  npm-look react vite -p git django --user npm js -p vue next -u express
   npm-look react vue vite @react/core
   npm-look --package react vue vite @react/core
   npm-look --user tj npm npmjs react
@@ -27,4 +29,36 @@ Examples:
 if (args.includes('--version') || args.includes('-v')) {
   console.log(pkg.version);
   process.exit(0);
+}
+
+const user_names: string[] = [];
+const package_names: string[] = [];
+
+let currentFlag: '-p' | '-u' = '-p';
+
+for (const arg of args) {
+  switch (arg) {
+    case '-p':
+    case '--package':
+      currentFlag = '-p';
+      break;
+
+    case '-u':
+    case '--user':
+      currentFlag = '-u';
+      break;
+
+    default:
+      if (!arg.startsWith('-')) {
+        (currentFlag === '-p' ? package_names : user_names).push(arg);
+      }
+  }
+}
+
+if (package_names.length > 0) {
+  await packageLook(package_names);
+}
+
+if (user_names.length > 0) {
+  await userLook(user_names);
 }
--- packages/npm-look/index.ts
+++ packages/npm-look/index.ts
@@ -31,12 +31,29 @@ export function variantsLook(
   ];
 }
 
+function toList(vals: string | string[]) {
+  return Array.isArray(vals) ? vals : [vals];
+}
+
+export async function getDataLook(name: string) {
+  const NPM_ENDPOINT = 'https://registry.npmjs.org/';
+
+  try {
+    const res = await fetch(NPM_ENDPOINT + name);
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.error((err as Error).message);
+  }
+}
+
 /**
  * Check that a username or array of usernames is valid.
  *
  * @throws {TypeError} If the argument is not a string or an array of strings.
  */
-export function userLook(
+export async function userLook(
   /** A string or an array of strings representing npm usernames. */
   username: string | string[],
 ) {
@@ -45,6 +62,21 @@ export function userLook(
       `Invalid argument: expected a string or an array of strings, but received ${typeof username}`,
     );
   }
+
+  const data = toList(username);
+
+  await Promise.all(
+    data.map(async (n) => {
+      if (!/^[a-z0-9-]+$/.test(n)) {
+        console.error(
+          'Invalid npm username:',
+          n,
+          '\nUsernames must be lowercase and can only contain letters, numbers, and dashes (-)',
+        );
+        return;
+      }
+    }),
+  );
 }
 
 /**
@@ -52,7 +84,7 @@ export function userLook(
  *
  * @throws {TypeError} If the argument is not a string or an array of strings.
  */
-export function packageLook(
+export async function packageLook(
   /** A string or an array of strings representing npm package names. */
   name: string | string[],
 ) {
@@ -61,4 +93,6 @@ export function packageLook(
       `Invalid argument: expected a string or an array of strings, but received ${typeof name}`,
     );
   }
+
+  const data = toList(name);
 }
